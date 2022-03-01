8fb39074e26d3de7dd7be80ab174dd2412a7033f
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-03-01 11:28:38 +0100

    Add basic structure and style
    -Added the basic html structure
    -Styled it with minimum fidelity

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.js'],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
