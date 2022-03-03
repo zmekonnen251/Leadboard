0ce2439b81d87c910d44978a3edda9af2419a185
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-03-03 12:19:48 +0100

    Add final touches

--- src/index.js
+++ src/index.js
@@ -6,26 +6,28 @@ const refreshBtn = document.querySelector('.refresh');
 const loadingText = document.querySelector('.loading');
 
 // const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-const urlWithId = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/K2w3YZM4MqdQ5Mz1JwCS/scores/';
+const urlWithId =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/K2w3YZM4MqdQ5Mz1JwCS/scores/';
 
 addScoreBtn.onclick = async (e) => {
   e.preventDefault();
   const userName = document.querySelector('.name').value;
   const userScore = document.querySelector('.score').value;
+  if (userName !== '' && userScore != '') {
+    await fetch(urlWithId, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        user: userName,
+        score: userScore,
+      }),
+    })
+      .then((result) => result.json())
+      .then((response) => response);
 
-  await fetch(urlWithId, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      user: userName,
-      score: userScore,
-    }),
-  })
-    .then((result) => result.json())
-    .then((response) => response);
-
-  document.querySelector('.name').value = '';
-  document.querySelector('.score').value = '';
+    document.querySelector('.name').value = '';
+    document.querySelector('.score').value = '';
+  }
 };
 
 const getScore = async () => {
